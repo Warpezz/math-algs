521123bafa275e55bcdcffb0ea82130586bbecb7
Warpezz <113236350+Warpezz@users.noreply.github.com>
2022-09-11 15:18:46 +0100

    finishing 2/3 of challenge 2
    + adding separate directory for the challenge

---
+++ GSCE_challenges/2_Speed_Tracker/NumberPlates.py
@@ -0,0 +1,26 @@
+#too many if statements i think but idk how else to do it; ill come back to this when im smorter
+
+def plate_checker(numPlate):
+    validPlate = True
+    if len(numPlate) != 7:
+        validPlate = False
+    else:
+        if numPlate[0:1].isalpha() == False:
+            validPlate = False
+        elif numPlate[2:3].isdigit() == False:
+            validPlate = False
+        elif numPlate[4:6].isalpha() == False:
+            validPlate = False
+
+    
+    
+    return validPlate
+
+numPlate = input("input a number plate of 7 alphanumeric characters\n")
+
+validPlate = plate_checker(numPlate)
+
+if validPlate:
+    print("Plate follows the naming scheme")
+else:
+    print("Plate is invalid")
